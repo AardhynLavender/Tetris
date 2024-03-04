9aa5d850508356ce157eadd01c8643ea983aee59
AardhynLavender <aardhyn@gmail.com>
2024-03-04 20:57:48 +1300

    feat: better screen size

--- src/constants/board.rs
+++ src/constants/board.rs
@@ -8,5 +8,5 @@ pub const TILE_PIECE_MARGIN: Size = 1; // margin between pieces in the tileset
 pub const SCREEN_CENTER: Vec2<i32> = Vec2::new(SCREEN_PIXELS.x as i32 / 2, SCREEN_PIXELS.y as i32 / 2);
 
 pub const BORDER_MARGIN: Size = 2;
-pub const BOARD_POSITION: Vec2<i32> = Vec2::new(SCREEN_CENTER.x - BOARD_DIMENSIONS.x as i32 * TILE_SIZE as i32 / 2, SCREEN_CENTER.y - BOARD_DIMENSIONS.y as i32 * TILE_SIZE as i32 / 2);
+pub const BOARD_POSITION: Vec2<i32> = Vec2::new(10, 10);
 pub const BOARD_DIMENSIONS: Size2 = Vec2::new(10, 20);
--- src/constants/window.rs
+++ src/constants/window.rs
@@ -3,5 +3,5 @@ use crate::application::render::color::{color, RGBA};
 
 pub const TITLE: &str = "Tetris";
 pub const SCREEN_COLOR: RGBA = color::MANTLE;
-pub const WINDOW_DIMENSIONS: Vec2<u32> = Vec2::new(1920, 1080);
-pub const SCREEN_PIXELS: Vec2<u32> = Vec2::new(384, 216);
+pub const WINDOW_DIMENSIONS: Vec2<u32> = Vec2::new(1000, 900);
+pub const SCREEN_PIXELS: Vec2<u32> = Vec2::new(200, 180);
